7252bf6a901240690ac70ad0f4e13811aa75d939
Wick007 <a.wick17@gmail.com>
2017-02-08 17:46:17 -0500

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,35 @@
-# code here!
+#1. create a class school that is initialize with a name
+
+
+class School
+  attr_reader :name, :roster
+
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+
+  def add_student(name, grade)
+    if !@roster.keys.include?(grade)
+      @roster[grade] =[]
+      @roster[grade] << name
+    else
+      @roster[grade] << name
+    end
+  end
+
+  def grade(grade)
+    return @roster[grade]
+  end
+
+  def sort
+    @roster.map do |k, v|
+      v.sort!
+    end
+    @roster
+  end
+end
+
+
+# school = School.new("Bayside High School")
+# school.roster # => {}
